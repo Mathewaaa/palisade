445d72cbad3a8d8c7a99070b84ec7f99270c4766
Mathewaaa <withdifferentletters@gmail.com>
2022-12-05 18:08:29 -0800

    yep

---
+++ Palisade/Views/Variable/FeedPostView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedPostView.swift
+//  Palisade
+//
+//  Created by Mathew Xie on 12/5/22.
+//
+
+import SwiftUI
+
+struct FeedPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedPostView()
+    }
+}
